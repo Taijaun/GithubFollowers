af5467e6daecb498ff4871c0071ff4394e23f77b
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-01-27 16:40:56 +0000

    initial commit, created tab bar vc, two nav vc's, and completed the ui for the search page

---
+++ GHFollowers/Components/GFTextField.swift
@@ -0,0 +1,43 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Taijaun Pitt on 27/01/2025.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        // make it fit text field
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+
+}
---
+++ GHFollowers/ViewControllers/FavouritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesListVC.swift
+//  GHFollowers
+//
+//  Created by Taijaun Pitt on 27/01/2025.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
