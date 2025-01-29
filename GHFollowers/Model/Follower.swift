b51fb1a4835cfd64698a14ac6c6193f8ae960b0e
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-01-29 23:06:45 +0000

    fixed a bug with the nav bar not appearing on followers vc when screen is dragged back to searchVC

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Taijaun Pitt on 29/01/2025.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
