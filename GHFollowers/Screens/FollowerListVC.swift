b96f173e91916b158314b4ca9acb58494fdc28b5
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-02-03 16:48:14 +0000

    created a function to update the collection view

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -13,6 +13,8 @@ class FollowerListVC: UIViewController {
         case main
     }
     
+    var followers: [Follower] = []
+    
     var username: String!
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -22,6 +24,7 @@ class FollowerListVC: UIViewController {
         configureCollectionView()
         configureViewController()
         getFollowers()
+        configureDataSource()
 
         
     }
@@ -62,12 +65,33 @@ class FollowerListVC: UIViewController {
             
             switch result {
             case .success(let followers):
-                print(followers)
+                self.followers = followers
+                self.updateData()
+                
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
 
         }
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        DispatchQueue.main.async{
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+        
     }
 
     
