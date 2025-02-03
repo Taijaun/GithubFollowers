fb5f1537cf020b8ff7180abcf1b9eedf3356d16a
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-02-03 16:37:47 +0000

    Created section enum for UICollectionviewDIffableDataSource generic

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String!
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
